1c35d4fbe283608253c29d4464c0fb294b18f145
ivangonza93 <ivangonza93@gmail.com>
2022-06-16 20:38:01 -0300

    login, signup,logour

--- Blog/settings.py
+++ Blog/settings.py
@@ -120,6 +120,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+LOGIN_REDIRECT_URL = 'home'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 instalar crispy forms
 
 'pip install django-crispy-forms'
+
+Paginas creadas
+
+admin/
+[name='home']
+sign_up/ 
